810669f8991a27756f3a27739d9d83ecedafdc71
ayushgrg0 <gurungayush982@gmail.com>
2025-06-21 20:05:01 +0545

    basics.py

--- 001_basics/basics _and_f_strings.py
+++ 001_basics/basics _and_f_strings.py
@@ -9,3 +9,11 @@ age = input("Enter your age:")
 education_qualification = input("Enter your education_qualification:")
 
 location = input("Enter your location:")
+
+print(f"""Hello {name}. /n
+      Here is your personal details ---> /n
+      {phone_number},  /n
+      {age}. /n
+      {education_qualification}. /n
+      {location}. /n
+      Welcome to Swift Academy. """)
