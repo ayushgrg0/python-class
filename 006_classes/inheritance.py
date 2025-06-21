66d66cdd3f8318487b064fea9dad048fc4cb77fa
ayushgrg0 <gurungayush982@gmail.com>
2025-06-21 20:34:51 +0545

    inheritance

---
+++ 006_classes/inheritance.py
@@ -0,0 +1,49 @@
+# Inheritance
+
+# instance == object
+
+
+class Janawaar:
+    def __init__(self, name: str):
+        self.name = name
+
+    def speak(self) -> str:
+        return ""
+
+
+class Dog(Janawaar):
+    def speak(self):
+        return f"{self.name}, barks!!"
+
+
+class Cat(Janawaar):
+    def speak(self):
+        return f"{self.name}, meows!!"
+
+
+dog = Dog("Kale")
+cat = Cat("Suri")
+
+print(dog.speak())
+print(cat.speak())
+
+
+class Teacher:
+    total_teacher = 0
+
+    def __init__(self, name) -> None:
+        self.name = name
+        Teacher.total_teacher += 1
+
+
+    @classmethod
+    def get_total_teacher(cls):
+        return cls.total_teacher
+
+
+teacher1 = Teacher("Ram")
+teacher2 = Teacher("Shyam")
+teacher3 = Teacher("Hari")
+
+
+print(Teacher.get_total_teacher())
