65caba606a64392998db0a06cd159f1a6c246a0a
ayushgrg0 <gurungayush982@gmail.com>
2025-06-21 20:02:44 +0545

    basics.py

---
+++ 001_basics/basics.py
@@ -0,0 +1,11 @@
+print("Hello World!!")
+
+name = input("Enter your name:")
+
+phone_number = input("Enter your phone number:")
+
+age = input("Enter your age:")
+
+education_qualification = input("Enter your education_qualification:")
+
+location = input("Enter your location:")
