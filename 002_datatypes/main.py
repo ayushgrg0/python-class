2f5c92bdc37d57bcfe96a24825bd30ca3e2e3e1b
ayushgrg0 <gurungayush982@gmail.com>
2025-06-21 20:24:24 +0545

    functions of hte datatype list

--- 002_datatypes/main.py
+++ 002_datatypes/main.py
@@ -23,3 +23,25 @@ data = [{
 }]
 
 print(data[0]['hobbies'][1]['address'][0]['home'][1]['lake'])
+
+
+'''
+gi
+'''
+
+
+fruits = ["Apple"]
+
+fruits.append("Orange")
+
+fruits[0] = "Litchi"  # update
+
+print(fruits)
+
+fruits.insert(1, "Banana")
+
+fruits.pop(0)
+
+# fruits.remove("Apple")
+
+print(fruits)
