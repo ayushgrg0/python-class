18e8c25256a59954152dc368711ceb73827e4259
ayushgrg0 <gurungayush982@gmail.com>
2025-06-21 20:31:35 +0545

    functions

--- 005_functions/functions.py
+++ 005_functions/functions.py
@@ -67,3 +67,74 @@ print(result)
 
 total, multiplication = calculate(4, 5)
 print(f'Sum : {total}, Product : {multiplication}')
+
+
+'''
+functions in a readable manner
+'''
+
+
+fruits = ["Apple"]
+
+
+def display_menu():
+    print("""
+__________________MENU__________________
+          1) Display fruits
+          2) Add fruits
+          3) Update fruits
+          4) Delete fruits
+          5) Exit
+    """)
+
+
+def display_fruits():
+    for fruit in fruits:
+        print(fruit)
+
+
+def add_fruits():
+    count = int(input("How many fruits do you want to add:"))
+    for i in range(1, count + 1):
+        fruit_name = input("Enter fruits")
+        fruits.append(fruit_name)
+
+    print("All the fruits are added successfully!!")
+
+
+def update_fruits():
+    display_fruits()
+    update_index = int(input("Enter the index to update:"))
+    update_value = input("Enter the updated fruit name")
+    fruits[update_index] = update_value
+    print("fruit successfully updated")
+
+
+def delete_fruits():
+    deletion_index = int(input("Enter the index to delete:"))
+    fruits.pop(deletion_index)
+    print(f"The item at index {deletion_index} is deleted")
+
+
+while True:
+    display_menu()
+
+    user_choice = input("Enter the choice:")
+    if user_choice == "1":
+        display_fruits()
+
+    elif user_choice == "2":
+        add_fruits()
+
+    elif user_choice == "3":
+        update_fruits()
+
+    elif user_choice == "4":
+        delete_fruits()
+
+    elif user_choice == "5":
+        print("Exiting Program")
+        break
+
+    else:
+        print("Invalid Choice")
