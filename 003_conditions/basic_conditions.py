c5c64292ae198705f9d6ee9caf45af25bd8551ee
ayushgrg0 <gurungayush982@gmail.com>
2025-06-21 20:12:05 +0545

    basic conditions

--- 003_conditions/basic_conditions.py
+++ 003_conditions/basic_conditions.py
@@ -5,4 +5,13 @@ if (user_age > 18):
 else:
     print("Incappable to vote.")
 
+user_input = int(input("Enter a number"))
+
+if user_input == 0:
+    print("Number is 0")
+elif user_input % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
+
 
