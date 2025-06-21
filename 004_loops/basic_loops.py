efe52d4f4ecdd42790b9a196135586b9c40047e9
ayushgrg0 <gurungayush982@gmail.com>
2025-06-21 20:17:26 +0545

    basic loops

---
+++ 004_loops/basic_loops.py
@@ -0,0 +1,23 @@
+
+# for loop
+
+fruits = ['apple', 'banana', 'kiwi', 'orange']
+
+for fruit in fruits:
+    print(fruit)
+
+for i in range(0, 1000):
+    print(f"{i + 1} Ayush Gurung")
+
+# while loop
+i = 0
+while i < 1000:
+
+    i += 1
+    if i == 5:
+        continue
+
+    print(f"{i}. Swift Academy")
+
+    if i == 100:
+        break
