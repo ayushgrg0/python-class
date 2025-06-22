60e08f26d87af10636ef3638e7a8efa0f61fb1d3
ayushgrg0 <gurungayush982@gmail.com>
2025-06-22 08:50:19 +0545

    File handilng

--- 008_file_handling/file_handling.py
+++ 008_file_handling/file_handling.py
@@ -6,8 +6,9 @@ printing a same data twice
 
 fp = open(r"C:\Users\USER\OneDrive\Desktop\python-class\008_file_handling\Hello.txt", "r")
 content = fp.read()
-fp.seek(0)
+fp.seek(0)  # the seek funtion is used to move the file cursor to a specific position inside a file
 print(content)
 
+# printing the file again
 value = fp.read()
 print(value)
