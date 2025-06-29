3f4aaf247769390bc89b0eaadf9b0ac1ae7fc6c2
ayushgrg0 <gurungayush982@gmail.com>
2025-06-29 10:37:05 +0545

    calculator.py module imported on main.py

---
+++ 012_modules/calculator.py
@@ -0,0 +1,15 @@
+def sum(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+# if __name__ == "__main__":
+#     print(f"This is a calculator module.{__name__}")
+#     print(sum(5, 5))
---
+++ 012_modules/main.py
@@ -0,0 +1,5 @@
+import calculator
+
+if __name__ == "__main__":
+    print("Name from runner", calculator.__name__)
+    print(calculator.sum(8, 9))
