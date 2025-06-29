5221ef7d16efe7745411ba8cb3150d0ea8bbd4f7
ayushgrg0 <gurungayush982@gmail.com>
2025-06-29 10:44:53 +0545

    Again new changes in 012 module

--- 012_modules/calculator.py
+++ 012_modules/calculator.py
@@ -12,3 +12,6 @@ def multiply(a, b):
 
 if __name__ == "__main__":  # font ligature
     print(f"This is a calculator module.{__name__}")
+    print(sum(8, 9))
+    print(multiply(8, 9))
+    print(subtract(8, 9))
--- 012_modules/main.py
+++ 012_modules/main.py
@@ -1,5 +1,8 @@
 import calculator
 
+print(__doc__)
+print(__file__)
+
 if __name__ == "__main__":
     print("Name from runner", __name__)
     print(calculator.sum(8, 9))
