6653fd97857e7022f40eef79da5177af1a6879a4
ayushgrg0 <gurungayush982@gmail.com>
2025-06-30 21:15:46 +0545

    Square of numbers using lambda

---
+++ Assignment/assignment_03/square_lambda.py
@@ -0,0 +1,19 @@
+'''
+Question 7. Square Lambda
+Define a lambda function that takes a number and returns its square. Assign it to a variable called square.
+Example:
+
+Input: square = lambda x: x * x; square(5)
+Output: 25
+Input: square(3)
+Output: 9
+'''
+
+# def func(number):
+#     square = number ** 2
+#     return square
+
+square = lambda x: x * x
+
+print(square(5))
+print(square(3))
