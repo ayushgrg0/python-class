cd5fee4bda1070e953fa74574b80f2947d3ebd81
ayushgrg0 <gurungayush982@gmail.com>
2025-07-01 20:10:36 +0545

    Assignment 0

--- Assignment/assignment_004/Dictionary Key-Value Inverter (Dictionaries).py
+++ Assignment/assignment_004/Dictionary Key-Value Inverter (Dictionaries).py
@@ -4,12 +4,15 @@ Write a function `invert_dict(d)` that inverts a dictionary, making keys into va
 If multiple keys have the same value, group those keys in a list as the new value.
 For example,`invert_dict({"a": 1, "b": 2, "c": 1})` returns `{1: ["a", "c"], 2: ["b"]}`.
 Use dictionary methods like `items()` and loops.
+.items() The items() method in Python dictionaries is used to
+retrieve a view object that displays a list of key-value pair tuples
 '''
 
 
 def invert_dict(d):
     inverted_dict = {}
-    for key, value in d.items():  # .items() The items() method in Python dictionaries is used to retrieve a view object that displays a list of key-value pair tuples
+
+    for key, value in d.items():
         if value in inverted_dict:
             inverted_dict[value].append(key)
         else:
---
+++ Assignment/assignment_004/Remove Duplicates from List (Lists and Loops).py
@@ -0,0 +1,19 @@
+'''
+Question 5. **Remove Duplicates from List (Lists and Loops)**
+Write a function `remove_duplicates(lst)` that removes duplicates from a list while preserving the original order.
+Do not use sets. For example, `remove_duplicates([1, 3, 3, 4, 1, 5])` returns `[1, 3, 4, 5]`.
+Use list methods like `append()` and a loop to check for duplicates.
+'''
+
+
+def remove_duplicates(lst):
+    no_duplicates = []
+    for i in lst:
+        if i in no_duplicates:
+            pass
+        else:
+            no_duplicates.append(i)
+    return no_duplicates
+
+
+print(remove_duplicates([1, 3, 3, 4, 1, 5]))
