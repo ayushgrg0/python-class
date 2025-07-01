f2c2f5215ca39c718cb6088cb1b575ffd832a62f
ayushgrg0 <gurungayush982@gmail.com>
2025-07-01 19:10:05 +0545

    Assignment 004 question 3

---
+++ Assignment/assignment_004/List Element Swapper (Lists and Loops).py
@@ -0,0 +1,23 @@
+'''
+Question 3. **List Element Swapper (Lists and Loops)**
+Write a function `swap_pairs(lst)` that takes a list and swaps every pair of adjacent elements.
+If the list has an odd length, the last element remains in place. For example, `swap_pairs([1, 2, 3, 4])`
+returns `[2, 1, 4, 3]`, and `swap_pairs([1, 2, 3])` returns `[2, 1, 3]`. Use list methods like indexing and loops.
+'''
+
+# a = 5
+# b = 4
+# print(a, b)
+
+# # a, b = b, a
+# print(b, a)
+
+
+def swap_pairs(lst):
+    for i in range(0, len(lst) - 1, 2):
+        lst[i], lst[i + 1] = lst[i + 1], lst[i]
+    return lst
+
+
+print(swap_pairs([1, 2, 3, 4]))
+print(swap_pairs([1, 2, 3]))
